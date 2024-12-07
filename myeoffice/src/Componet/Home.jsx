5b965f1ed4b0c15c9e087d16ed99a01bb12acec6
payalpatani <payalpatani83@gmail.com>
2024-12-07 14:02:34 +0530

    Added all files

--- myeoffice/src/Componet/Home.jsx
+++ myeoffice/src/Componet/Home.jsx
@@ -7,12 +7,27 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 export default function Home() {
   return (
     <div>
-      <div className="container-fulid">
-            <div className="row">
-                    <div className="col-lg-6 col-md-6 col-sm-12"></div>
-                    <div className="col-lg-6 col-md-6 col-sm-12"></div>
-            </div>
+       <div className="hero-section">
+      <div className="hero-content">
+        <h1>
+          Document <span>Management</span> System
+        </h1>
+        <p>
+          Our document management system software is designed to simplify the
+          way you manage your documents in the Office. With our solution, you
+          can easily access your important documents from anywhere, at any
+          time, and on any device.
+        </p>
+        <div className="hero-buttons">
+          <button className="btn-primary">Log In</button>
+          <span>OR</span>
+          <button className="btn-secondary">Book Your Demo</button>
+        </div>
       </div>
+      <div className="hero-image">
+        <img src="https://myeoffice.in/wp-content/uploads/2024/06/kindpng_1247377-1536x880-1.webp" alt="Document Management Illustration" />
+      </div>
+    </div>
     </div>
   )
 }
